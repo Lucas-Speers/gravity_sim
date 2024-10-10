88d2ee1a296ec50979f1e7080f97da7c1a53b7f8
Lucas Speers <lucas_speers@outlook.com>
2024-10-10 10:55:45 -0600

    It should be working now

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <math.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <time.h>
 
 typedef int bool;
-int quad_capacity = 1000;
+int quad_capacity = 50;
+// TODO: why does a value of 19362 or more break on windows???
+int point_count = 100000;
+float G = 0;
+
+bool width_is_max;
 
 void expect(void *p) {
     if (p == NULL) {
-        fprintf(stderr, "Bad alloc");
+        printf("Bad alloc\n");
         exit(1);
     }
 }
@@ -19,10 +22,9 @@ float rand_float() {
     return (float)rand()/(float)(RAND_MAX);
 }
 
-struct Point {
+typedef struct Point {
     float x, y, vx, vy;
-};
-typedef struct Point Point;
+} Point;
 
 void new_point(Point *p, float x, float y) {
     p->x = x;
@@ -30,25 +32,23 @@ void new_point(Point *p, float x, float y) {
     p->vx = p->vy = 0;
 }
 
-struct Rectangle {
+typedef struct Rectangle {
     float x, y, w, h;
-};
-typedef struct Rectangle Rectangle;
+} Rectangle;
 
 bool rect_contains_point(Rectangle *r, Point *p) {
     return (r->x <= p->x) & (p->x < (r->x+r->w)) &
            (r->y <= p->y) & (p->y < (r->y+r->h));
 }
 
-struct QuadTree {
+typedef struct QuadTree {
     Rectangle bounds;
     int mass; // number of points in all child trees
     
     bool is_branch; // whether this is split or not
     void *ptr; // ethier a list of points or four quad tree branches
     int points; // stores the number of points if leave node
-};
-typedef struct QuadTree QuadTree;
+} QuadTree;
 
 void new_quad_tree(QuadTree *qt) {
     qt->mass = 0;
@@ -154,12 +154,27 @@ void free_quad_tree(QuadTree *qt) {
     free(qt->ptr);
 }
 
-struct VecPointers {
+void get_points_from_tree(QuadTree *qt, Point *array, int *index) {
+    if (qt->is_branch) {
+        QuadTree *children = qt->ptr;
+        get_points_from_tree(&children[0], array, index);
+        get_points_from_tree(&children[1], array, index);
+        get_points_from_tree(&children[2], array, index);
+        get_points_from_tree(&children[3], array, index);
+    } else {
+        Point *points = qt->ptr;
+        for (int i=0; i<qt->points; i++) {
+            array[*index] = points[i];
+            *index += 1;
+        }
+    }
+}
+
+typedef struct VecPointers {
     QuadTree **ptr;
     int size;
     int capacity;
-};
-typedef struct VecPointers VecPointers;
+} VecPointers;
 
 void vec_with_capacity(VecPointers *vec, int capacity) {
     vec->ptr = calloc(capacity, sizeof(QuadTree*));
@@ -174,9 +189,7 @@ QuadTree** vec_push(VecPointers *vec) {
         vec->ptr = realloc(vec->ptr, vec->capacity * sizeof(QuadTree*));
         expect(vec->ptr);
     }
-    // printf("pointer1: %p\n", vec->ptr);
     QuadTree **ptr = &(vec->ptr[vec->size]);
-    // printf("pointer2: %p\n", ptr);
     vec->size++;
     return ptr;
 }
@@ -199,6 +212,10 @@ float distance(float x, float y, float a, float b) {
     return sqrtf(((x+a)*(x+a))+((y+b)*(y+b)));
 }
 
+float distance_sqrd(float x, float y, float a, float b) {
+    return sqrtf(((x+a)*(x+a))+((y+b)*(y+b)));
+}
+
 void update_point(Point *p, QuadTree *root) {
     VecPointers stack;
     vec_with_capacity(&stack, 50);
@@ -208,14 +225,28 @@ void update_point(Point *p, QuadTree *root) {
     vec_with_capacity(&next_stack, stack.size);
     while (1) {
         for (int i=0; i<stack.size; i++) {
-            if (get_vec(&stack, i)->is_branch) {
-                // check if it's acceptable TODO
-                float acceptability = distance(p->x, p->y, get_vec(&stack, i)->bounds.x+(get_vec(&stack, i)->bounds.w/2.0), get_vec(&stack, i)->bounds.y+(get_vec(&stack, i)->bounds.h/2.0)) / max_int(get_vec(&stack, i)->bounds.w, get_vec(&stack, i)->bounds.h);
+            QuadTree *current_qt = get_vec(&stack, i);
+            if (current_qt->is_branch) {
+                // check if it's acceptable
+                float center_x = current_qt->bounds.x+(current_qt->bounds.w/2.0);
+                float center_y = current_qt->bounds.y+(current_qt->bounds.h/2.0);
+                float size;
+                if (width_is_max) {
+                    size = current_qt->bounds.w;
+                } else {
+                    size = current_qt->bounds.h;
+                }
+                float acceptability = distance(p->x, p->y, center_x, center_y) / size;
                 if (acceptability < 0.5) {
                     // do calculations for the entire mass
+                    float r = 1.0/distance_sqrd(p->x, p->y, center_x, center_y);
+                    float x_part = fabsf(p->x - center_x);
+                    float y_part = fabsf(p->y - center_y);
+                    p->vx += r*x_part * G * current_qt->mass;
+                    p->vy += r*y_part * G * current_qt->mass;
                 } else {
-                    // otherwise just add it's children to the stack TODO
-                    QuadTree *children = get_vec(&stack, i)->ptr;
+                    // otherwise just add it's children to the stack
+                    QuadTree *children = current_qt->ptr;
                     for (int x=0; x<4; x++) {
                         QuadTree **pointer = vec_push(&next_stack);
                         *pointer = &children[x];
@@ -223,11 +254,18 @@ void update_point(Point *p, QuadTree *root) {
                 }
             } else {
                 // do all the calculations with the points TODO
+                Point *points = current_qt->ptr;
+                for (int x=0; x<current_qt->points; x++) {
+                    float r = 1.0/distance_sqrd(p->x, p->y, points[x].x, points[x].y);
+                    float x_part = fabsf(p->x - points[x].x);
+                    float y_part = fabsf(p->y - points[x].y);
+                    p->vx += r*x_part * G;
+                    p->vy += r*y_part * G;
+                }
             }
         }
         // if there are no new jobs, break
         if (stack.size == 0) {
-            // printf("z\n");
             clear_vec(&stack);
             clear_vec(&next_stack);
             break;
@@ -256,15 +294,18 @@ void update_velocities(QuadTree *root, QuadTree *current) {
 }
 
 int main() {
-    printf("Starting...\n");
+    time_t start, stop;
+    start = clock();
+    printf("Starting...");
+    fflush(stdout);
     
     QuadTree root;
     new_quad_tree(&root);
-    
     root.bounds.x = root.bounds.y = -1.0;
     root.bounds.w = root.bounds.h = 2.0;
+    width_is_max = 1;
     
-    for (int i=0; i<1000000; i++) {
+    for (int i=0; i<point_count; i++) {
         float x = rand_float()*2.0-1.0;
         float y = rand_float()*2.0-1.0;
         Point p;
@@ -273,12 +314,31 @@ int main() {
         p.vx = p.vy = 0;
         add_quad_tree_point(&root, &p);
     }
+    printf("Done\n");
     
     printf("Depth: %d\n", max_depth(&root));
 
+    printf("Updating Velocities...");
+    fflush(stdout);
+
     update_velocities(&root, &root);
+    printf("Done\n");
+
+    printf("Recollecting...");
+    fflush(stdout);
+
+    Point *points = calloc(point_count, sizeof(Point));
+    expect(points);
+    int index = 0;
+    get_points_from_tree(&root, points, &index);
+    printf("Done\n");
     
     free_quad_tree(&root);
+    free(points);
+
+    stop = clock();
+
+    printf("time: %f\n", (float)(stop-start)/CLOCKS_PER_SEC);
     
     return 0;
 }
